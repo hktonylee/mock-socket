d6114d71eaf5abd9f2287a39a789d90174e4669b
Tony Lee <tlee@lfl.hk>
2017-09-11 11:45:11 +0800

    fix the get onerror problem. it should be function type

--- src/websocket.js
+++ src/websocket.js
@@ -47,7 +47,7 @@ class WebSocket extends EventTarget {
         configurable: true,
         enumerable: true,
         get() {
-          return this.listeners.open;
+          return this.listeners.open[0];
         },
         set(listener) {
           this.addEventListener('open', listener);
@@ -57,7 +57,7 @@ class WebSocket extends EventTarget {
         configurable: true,
         enumerable: true,
         get() {
-          return this.listeners.message;
+          return this.listeners.message[0];
         },
         set(listener) {
           this.addEventListener('message', listener);
@@ -67,7 +67,7 @@ class WebSocket extends EventTarget {
         configurable: true,
         enumerable: true,
         get() {
-          return this.listeners.close;
+          return this.listeners.close[0];
         },
         set(listener) {
           this.addEventListener('close', listener);
@@ -77,7 +77,7 @@ class WebSocket extends EventTarget {
         configurable: true,
         enumerable: true,
         get() {
-          return this.listeners.error;
+          return this.listeners.error[0];
         },
         set(listener) {
           this.addEventListener('error', listener);
